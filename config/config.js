28b13d1403dbfe952268540df2da91f2e61382f7
Ana <sarriaarceanamaria18@gmail.com>
2025-03-04 14:40:55 -0500

    conexion render

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'postgresql://flutterAu_owner:npg_LUW3xgAKds7r@ep-wispy-bar-a5oocbpq-pooler.us-east-2.aws.neon.tech/flutterAu?sslmode=require',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
@@ -14,12 +14,12 @@ module.exports = {
   test: {
     username: 'root',
     password: null,
-    database: 'database_test',
+    database: 'AutosBackend',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'postgresql://flutterAu_owner:npg_LUW3xgAKds7r@ep-wispy-bar-a5oocbpq-pooler.us-east-2.aws.neon.tech/flutterAu?sslmode=require',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
