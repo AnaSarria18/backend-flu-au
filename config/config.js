14e57d0069da38f35c65a9978b8915d0a2b0826e
Ana <sarriaarceanamaria18@gmail.com>
2025-03-04 15:14:39 -0500

    cambio jsconfig

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    use_env_variable: 'postgresql://flutterAu_owner:npg_LUW3xgAKds7r@ep-wispy-bar-a5oocbpq-pooler.us-east-2.aws.neon.tech/flutterAu?sslmode=require',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
@@ -14,12 +14,12 @@ module.exports = {
   test: {
     username: 'root',
     password: null,
-    database: '',
+    database: 'database_test',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
   production: {
-    use_env_variable: '',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
